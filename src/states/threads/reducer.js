2f049434e21a151b7034263b5b545bea293eba5e
Faidil <faidilfamozzy@gmail.com>
2024-02-27 23:17:42 +0800

    refactor: refactor threads state reducer

--- src/states/threads/reducer.js
+++ src/states/threads/reducer.js
@@ -9,41 +9,33 @@ export default function threadsReducer(threads = [], action = {}) {
     case actionType.UPVOTE_THREAD:
       return threads.map((thread) => {
         if (thread.id === action.payload.threadId) {
-          const updatedThread = { ...thread }
-          updatedThread.upVotesBy = [...thread.upVotesBy, action.payload.userId]
-          if (thread.downVotesBy.includes(action.payload.userId)) {
-            updatedThread.downVotesBy = thread.downVotesBy.filter((id) => id !== action.payload.userId)
+          return {
+            ...thread,
+            upVotesBy: [...thread.upVotesBy, action.payload.userId],
+            downVotesBy: thread.downVotesBy.filter((id) => id !== action.payload.userId)
           }
-          return updatedThread
         }
         return thread
       })
     case actionType.DOWNVOTE_THREAD:
       return threads.map((thread) => {
         if (thread.id === action.payload.threadId) {
-          const updatedThread = { ...thread }
-          updatedThread.downVotesBy = [...thread.downVotesBy, action.payload.userId]
-          if (thread.upVotesBy.includes(action.payload.userId)) {
-            updatedThread.upVotesBy = thread.upVotesBy.filter((id) => id !== action.payload.userId)
+          return {
+            ...thread,
+            downVotesBy: [...thread.downVotesBy, action.payload.userId],
+            upVotesBy: thread.upVotesBy.filter((id) => id !== action.payload.userId)
           }
-          return updatedThread
         }
         return thread
       })
     case actionType.UNVOTE_THREAD:
       return threads.map((thread) => {
-        const { upVotesBy: upVotes, downVotesBy: downVotes } = thread
-        const isUpVoted = upVotes.includes(action.payload.userId)
-        const isDownVoted = downVotes.includes(action.payload.userId)
         if (thread.id === action.payload.threadId) {
-          const updatedThread = { ...thread }
-          if (isUpVoted) {
-            updatedThread.upVotesBy = upVotes.filter((id) => id !== action.payload.userId)
+          return {
+            ...thread,
+            upVotesBy: thread.upVotesBy.filter((id) => id !== action.payload.userId),
+            downVotesBy: thread.downVotesBy.filter((id) => id !== action.payload.userId)
           }
-          if (isDownVoted) {
-            updatedThread.downVotesBy = downVotes.filter((id) => id !== action.payload.userId)
-          }
-          return updatedThread
         }
         return thread
       })
