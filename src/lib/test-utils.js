10a6f4d4732ce3dceb8e09a0fb8fc661523fd1b5
Faidil <faidilfamozzy@gmail.com>
2024-03-11 18:14:48 +0800

    ci: test ci action

--- src/lib/test-utils.js
+++ src/lib/test-utils.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-// import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import { setupStore } from '../states'
 
